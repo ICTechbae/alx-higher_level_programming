8dddc44da4eeedace237a2884e52a669ea8b96ca
iwuohachioma30@gmail.com <iwuohachioma30@gmail.com>
2024-02-06 06:01:11 -0800

    0x11-python-network_1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,8 @@ requests model
 """
 
 if __name__ == '__main__':
-        import requests
-            html = requests.get('https://alx-intranet.hbtn.io/status')
-                print("Body response:")
-                    print("\t- type: {}".format(html.text.__class__))
-                        print("\t- content: {}".format(html.text))
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
